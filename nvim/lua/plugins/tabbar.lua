d6598602bb6a1812d7a5e460daa3da59bebc9511
Joao Esquioga <joao.esquioga@klarna.com>
2025-03-20 09:41:06 +0100

    disabled lazy load for tabbar

--- nvim/lua/plugins/tabbar.lua
+++ nvim/lua/plugins/tabbar.lua
@@ -4,6 +4,7 @@ return {
     'lewis6991/gitsigns.nvim', 
     'nvim-tree/nvim-web-devicons', 
   },
+  lazy = false,
   init = function() vim.g.barbar_auto_setup = false end,
   keys = {
     { '<leader>bc', ':BufferClose<CR>', desc = 'Close current buffer' },
@@ -12,11 +13,7 @@ return {
     { '<leader>bj', ':BufferPick<CR>', desc = 'Jump to buffer' },
   },
   opts = {
-    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-    -- animation = true,
-    -- insert_at_start = true,
-    -- …etc.
-
+    insert_at_start = true,
   },
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
